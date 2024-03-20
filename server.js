eaa1ec26e658df85898b339b8c18394d07a78c14
bq9605 <bdv.villan@gmail.com>
2024-03-20 10:04:36 +0800

    [updated] with collectionId, itemId

--- server.js
+++ server.js
@@ -6,48 +6,49 @@ const bodyParser = require("body-parser");
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
-
-app.get("/api/getAmounts", async (req, res) => {
+const apiToken =
+  "cc5d048b20458561d8f5a15c42c26cf537f4bc67075188b2b399dcaa7173cce7";
+app.post("/api/getAmount", async (req, res) => {
   try {
+    const { collectionId, itemId } = req.body;
     const response = await axios.get(
-      "https://api.webflow.com/v2/collections/65f5c4206c7db01c7173f20f/items",
+      "https://api.webflow.com/v2/collections/" +
+        collectionId +
+        "/items/" +
+        itemId,
       {
         headers: {
-          Authorization:
-            "Bearer cc5d048b20458561d8f5a15c42c26cf537f4bc67075188b2b399dcaa7173cce7",
+          Authorization: "Bearer " + apiToken,
           "Content-Type": "application/json",
           // Add more headers as needed
         },
       }
     );
     const data = response.data;
-    res.json(data);
+    res.json({ bagelamount: data.fieldData.amount });
   } catch (error) {
     console.error("Error:", error);
     res.status(500).json({ error: "An error occurred" });
   }
 });
-app.post("/api/setAmounts", async (req, res) => {
+app.post("/api/setAmount", async (req, res) => {
   try {
-    const { totalAmount, currentAmount } = req.body;
-    console.log(
-      "🚀 ~ app.post ~ totalAmount, currentAmount:",
-      totalAmount,
-      currentAmount
-    );
+    const { bagelamount, collectionId, itemId } = req.body;
     const response = await axios.patch(
-      "https://api.webflow.com/v2/collections/65f5c4206c7db01c7173f20f/items/65f5cadfaf79783925609d21",
+      "https://api.webflow.com/v2/collections/" +
+        collectionId +
+        "/items/" +
+        itemId,
       {
         fieldData: {
-          "total-amount": totalAmount,
-          "current-amount": currentAmount,
+          amount: bagelamount,
         },
       },
       {
         headers: {
-          Authorization:
-            "Bearer cc5d048b20458561d8f5a15c42c26cf537f4bc67075188b2b399dcaa7173cce7",
+          Authorization: "Bearer " + apiToken,
           "Content-Type": "application/json",
+          Accept: "application/json",
           // Add more headers as needed
         },
       }
